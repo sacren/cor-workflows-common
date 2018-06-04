659f2ecdc5b168b5fbfeb760be3675dc6c977dec
BJ Neilsen <bj.neilsen@gmail.com>
2018-06-04 15:41:31 -0400

    Add category data type with tests

---
+++ src/data-dictionary/data-types/category.js
@@ -0,0 +1,28 @@
+import _ from 'lodash'
+import { CATEGORY, TEXT } from '../return-types'
+import {
+  IS,
+  CONTAINS,
+  DOES_NOT_CONTAIN,
+  BEGINS_WITH,
+  ENDS_WITH,
+  DOES_NOT_BEGIN_WITH,
+  DOES_NOT_END_WITH
+} from '../operators'
+
+export const TYPE = CATEGORY
+export const CAN_COERCE_TO = [CATEGORY, TEXT]
+export const VALID_OPERATORS = {
+  [IS]: [CATEGORY, TEXT],
+  [CONTAINS]: [TEXT],
+  [DOES_NOT_CONTAIN]: [TEXT],
+  [BEGINS_WITH]: [TEXT],
+  [ENDS_WITH]: [TEXT],
+  [DOES_NOT_BEGIN_WITH]: [TEXT],
+  [DOES_NOT_END_WITH]: [TEXT]
+}
+
+export const COERCIONS = {
+  [CATEGORY]: _.identity,
+  [TEXT]: category => category.displayName || category.name
+}
