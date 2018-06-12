9a0a2a15601771de28d2f23218cb818b8fcce787
Thomas A. Valletta <tom@vallettas.com>
2018-06-12 10:21:06 -0600

    just beginning

---
+++ src/data-dictionary/data-types/group-set.spec.js
@@ -0,0 +1,57 @@
+import { CATEGORY, GROUP, TEXT } from '../return-types'
+import { CAN_COERCE_TO, COERCIONS, TYPE } from './group'
+
+describe('Group Data Type', () => {
+  describe('TYPE', () => {
+    it('is CATEGORY', () => {
+      expect(TYPE).toBe(GROUP)
+    })
+  })
+
+  describe('CAN_COERCE_TO', () => {
+    it('can coerce to CATEGORY', () => {
+      expect(CAN_COERCE_TO).toContain(CATEGORY)
+      expect(COERCIONS).toHaveProperty(CATEGORY)
+      expect(COERCIONS[CATEGORY].length).toBe(1)
+    })
+
+    it('can coerce to GROUP', () => {
+      expect(CAN_COERCE_TO).toContain(GROUP)
+      expect(COERCIONS).toHaveProperty(GROUP)
+      expect(COERCIONS[GROUP].length).toBe(1)
+    })
+
+    it('can coerce to TEXT', () => {
+      expect(CAN_COERCE_TO).toContain(TEXT)
+      expect(COERCIONS).toHaveProperty(TEXT)
+      expect(COERCIONS[TEXT].length).toBe(1)
+    })
+  })
+
+  describe('COERCIONS', () => {
+    it('coerces group -> category', () => {
+      const group = { displayName: 'Bob', categoryId: 'cat-123' }
+      const expected = { id: group.categoryId }
+      const coercedGroup = COERCIONS[CATEGORY](group)
+      expect(coercedGroup).toEqual(expected)
+    })
+
+    it('coerces group -> group (e.g. identity)', () => {
+      const group = { displayName: 'Bob' }
+      const coercedGroup = COERCIONS[GROUP](group)
+      expect(coercedGroup).toEqual(group)
+    })
+
+    it("coerces group -> text using the group's displayName", () => {
+      const group = { displayName: 'Bobby', name: 'Robert' }
+      const coercedGroup = COERCIONS[TEXT](group)
+      expect(coercedGroup).toEqual(group.displayName)
+    })
+
+    it("coerces group -> text using the group's name when the displayName is not present", () => {
+      const group = { displayName: undefined, name: 'Robert' }
+      const coercedGroup = COERCIONS[TEXT](group)
+      expect(coercedGroup).toEqual(group.name)
+    })
+  })
+})
--- src/data-dictionary/data-types/index.js
+++ src/data-dictionary/data-types/index.js
@@ -4,11 +4,13 @@ import {
   CATEGORY,
   FORM,
   GROUP,
+  GROUP_SET,
   NUMBER,
   OPTIONS,
   ROLE,
   SUBFLOW,
   TEXT,
+  TEXT_SET,
   USER
 } from '../return-types'
 import * as all from './all'
@@ -16,11 +18,13 @@ import * as boolean from './boolean'
 import * as category from './category'
 import * as form from './form'
 import * as group from './group'
+import * as groupSet from './group-set'
 import * as number from './number'
 import * as options from './options'
 import * as role from './role'
 import * as subflow from './subflow'
 import * as text from './text'
+import * as textSet from './text-set'
 import * as user from './user'
 
 export default {
@@ -30,10 +34,12 @@ export default {
   [FORM]: form,
   [FORM]: form,
   [GROUP]: group,
+  [GROUP_SET]: groupSet,
   [NUMBER]: number,
   [OPTIONS]: options,
   [ROLE]: role,
   [SUBFLOW]: subflow,
   [TEXT]: text,
+  [TEXT_SET]: textSet,
   [USER]: user
 }
