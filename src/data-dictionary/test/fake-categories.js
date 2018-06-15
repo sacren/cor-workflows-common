96b9f2d393e1d7f9b0b116f154730ef04b45bd83
BJ Neilsen <bj.neilsen@gmail.com>
2018-06-15 17:27:34 -0400

    Add support for categories/category context mocks in tests

---
+++ src/data-dictionary/test/fake-categories.js
@@ -0,0 +1,219 @@
+/* Copyright © 2016 Kuali, Inc. - All Rights Reserved
+ * You may use and modify this code under the terms of the Kuali, Inc.
+ * Pre-Release License Agreement. You may not distribute it.
+ *
+ * You should have received a copy of the Kuali, Inc. Pre-Release License
+ * Agreement with this file. If not, please write to license@kuali.co.
+ */
+export const CATEGORIES = [
+  {
+    createdAt: '2015-08-21T22:11:06.819Z',
+    updatedAt: '2017-04-02T19:20:55.366Z',
+    updatedBy: {
+      id: '1250642154315750499',
+      impersonatedBy: null
+    },
+    id: 'f6da4d4a-b490-4172-9e54-07b56ab1fc4d',
+    newId: '5723833601089f965098b71c',
+    parentId: '',
+    startDate: '1970-01-01T00:00:00.000Z',
+    relationshipSchemas: [],
+    roleSchemas: [
+      {
+        name: 'College Curriculum Committee',
+        id: 'EysWRQdh',
+        description: ''
+      },
+      {
+        name: 'Dean',
+        id: 'NyaBoBep',
+        description: ''
+      },
+      {
+        name: 'Administrator',
+        id: 'V1TydEZC',
+        description: ''
+      }
+    ],
+    name: 'College',
+    fieldSchemas: [
+      {
+        id: '4JBIsHea',
+        type: 'text',
+        name: 'Phone'
+      }
+    ],
+    versionId: '57f4988a8946d351523d565d'
+  },
+  {
+    createdAt: '2015-08-21T03:54:08.037Z',
+    updatedAt: '2017-04-02T19:20:23.641Z',
+    updatedBy: {
+      id: '1250642154315750499',
+      impersonatedBy: null
+    },
+    id: '119f7a58-e1fc-413a-a142-4aee79bce1dd',
+    newId: '5723833601089f965098b710',
+    parentId: 'f6da4d4a-b490-4172-9e54-07b56ab1fc4d',
+    startDate: '1970-01-01T00:00:00.000Z',
+    relationshipSchemas: [],
+    roleSchemas: [
+      {
+        name: 'Chair',
+        id: 'VJgLTsS3',
+        description: ''
+      },
+      {
+        name: 'Curriculum Manager',
+        id: 'V1MdR8Wa',
+        description: ''
+      },
+      {
+        name: 'Distance Learning Committee',
+        id: '4kH7SC0og',
+        description: ''
+      }
+    ],
+    name: 'Department',
+    fieldSchemas: [
+      {
+        id: 'N1Lqvqkae',
+        type: 'text',
+        name: 'Web site'
+      },
+      {
+        id: 'VkK5P9kpx',
+        type: 'text',
+        name: 'Phone'
+      },
+      {
+        id: 'NJlXth4kb',
+        type: 'checkbox',
+        name: 'Pilot'
+      }
+    ],
+    versionId: '57f4988a8946d351523d565c'
+  },
+  {
+    updatedAt: '2017-07-17T20:21:44.924Z',
+    createdAt: '2016-12-15T12:17:32.606Z',
+    updatedBy: {
+      id: '1265960897903199194',
+      impersonatedBy: 'ldap:1646100522'
+    },
+    id: '585289dcb667497182a3d9ee',
+    newId: '585289dcb667497182a3d9ee',
+    parentId: 'f3b9d0dc-2a39-48aa-b5d4-2ddbd458a25f',
+    startDate: '1970-01-01T00:00:00.000Z',
+    relationshipSchemas: [],
+    roleSchemas: [
+      {
+        name: 'Dean',
+        id: 'rJKkcbg4g',
+        description: ''
+      },
+      {
+        name: 'PVC',
+        id: 'rkZlq-lVl',
+        description: ''
+      },
+      {
+        name: 'FAQSC',
+        id: 'r1x-x4lNe',
+        description: ''
+      },
+      {
+        name: 'Curriculum Manager',
+        id: 'BJ2m35qBZ',
+        description: ''
+      }
+    ],
+    name: 'Faculty',
+    fieldSchemas: [
+      {
+        id: 'Bk0Sn99HW',
+        type: 'text',
+        name: 'Phone Number'
+      },
+      {
+        id: 'rJIUnc5BW',
+        type: 'text',
+        name: 'Address'
+      }
+    ],
+    versionId: '585289dcb667497182a3d9ef'
+  },
+  {
+    updatedAt: '2016-12-12T16:03:07.381Z',
+    createdAt: '2016-12-12T16:02:38.719Z',
+    updatedBy: {
+      id: '1265960897903199194',
+      impersonatedBy: null
+    },
+    id: '584eca1eb667497182a3d2f4',
+    newId: '584eca1eb667497182a3d2f4',
+    parentId: null,
+    startDate: '1970-01-01T00:00:00.000Z',
+    relationshipSchemas: [],
+    roleSchemas: [
+      {
+        name: 'Head',
+        id: 'HylEqSnXl',
+        description: ''
+      },
+      {
+        name: 'Deputy QA',
+        id: 'rkwNqSn7x',
+        description: ''
+      }
+    ],
+    name: 'School',
+    fieldSchemas: [],
+    versionId: '584eca1eb667497182a3d2f5'
+  },
+  {
+    updatedAt: '2017-11-16T05:46:11.430Z',
+    createdAt: '2017-11-16T05:46:00.055Z',
+    updatedBy: {
+      id: '57715a01e59658185841a030',
+      impersonatedBy: 'ldap:1646150502'
+    },
+    id: '5a0d2618fc545000019a0405',
+    newId: '5a0d2618fc545000019a0405',
+    parentId: null,
+    startDate: '1970-01-01T00:00:00.000Z',
+    relationshipSchemas: [],
+    roleSchemas: [],
+    name: 'Space Station',
+    fieldSchemas: [],
+    versionId: '5a0d2618fc545000019a0406'
+  },
+  {
+    createdAt: '2015-08-27T16:47:05.905Z',
+    updatedAt: '2015-08-27T16:47:05.905Z',
+    updatedBy: {
+      id: null,
+      impersonatedBy: null
+    },
+    id: 'f3b9d0dc-2a39-48aa-b5d4-2ddbd458a25f',
+    newId: '5723833601089f965098b732',
+    parentId: null,
+    startDate: '1970-01-01T00:00:00.000Z',
+    relationshipSchemas: [],
+    roleSchemas: [
+      {
+        name: 'Provost',
+        id: 'V114RX_n',
+        description: ''
+      },
+      {
+        name: 'UC Commitee',
+        id: 'NJHEtiH7g',
+        description: ''
+      }
+    ],
+    name: 'University',
+    fieldSchemas: [],
+    versionId: '57f4988a8946d351523d565e'
+  }
+]
