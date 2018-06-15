f5221202fb5e8162458e9d102fdd5b1f2fff3d2e
BJ Neilsen <bj.neilsen@gmail.com>
2018-06-15 17:29:50 -0400

    Extract test runner function for shorter/readable setup+invocation

--- src/data-dictionary/operators/contains.spec.js
+++ src/data-dictionary/operators/contains.spec.js
@@ -3,29 +3,33 @@ import { getMockTextContext } from '../test/fake-context'
 import { runOperatorTest } from '../test/util'
 
 describe('contains operator', () => {
-  test('text text true', async () => {
-    const leftFn = getMockTextContext.bind(this, 'testing')
-    const rightFn = getMockTextContext.bind(this, 'esti')
-    const response = await runOperatorTest(
-      CONTAINS,
-      leftFn,
-      'testing',
-      rightFn,
-      'esti'
-    )
-    expect(response).toBe(true)
   })
 
-  test('text text false', async () => {
-    const leftFn = getMockTextContext.bind(this, 'testing')
-    const rightFn = getMockTextContext.bind(this, 'fail')
-    const response = await runOperatorTest(
-      CONTAINS,
-      leftFn,
-      'testing',
-      rightFn,
-      'fail'
-    )
-    expect(response).toBe(false)
+  describe('TEXT:TEXT', () => {
+    test('text contains text', async () => {
+      const response = await runEvaluation(
+        getMockTextContext,
+        getMockTextContext,
+        'testing',
+        'esti'
+      )
+      expect(response).toBe(true)
+    })
+
+    test('text does not contain', async () => {
+      const response = await runEvaluation(
+        getMockTextContext,
+        getMockTextContext,
+        'testing',
+        'fail'
+      )
+      expect(response).toBe(false)
+    })
   })
+
+  async function runEvaluation (leftMockFn, rightMockFn, left, right) {
+    const leftFn = leftMockFn.bind(this, left)
+    const rightFn = rightMockFn.bind(this, right)
+    return runOperatorTest(CONTAINS, leftFn, left, rightFn, right)
+  }
 })
