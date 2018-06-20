11b2884f81838b5f3e5ba417adcf7613ecac725d
BJ Neilsen <bj.neilsen@gmail.com>
2018-06-20 17:54:23 -0400

    Missed is-not-empty unary type re-organization

--- src/data-dictionary/operators/is-not-empty.js
+++ src/data-dictionary/operators/is-not-empty.js
@@ -2,10 +2,6 @@ import _ from 'lodash'
 import { GROUP_LIST, TEXT } from '../return-types'
 
 export default {
-  [GROUP_LIST]: {
-    [undefined]: _.negate(_.isEmpty)
-  },
-  [TEXT]: {
-    [undefined]: _.negate(_.isEmpty)
-  }
+  [GROUP_LIST]: _.negate(_.isEmpty),
+  [TEXT]: _.negate(_.isEmpty)
 }
