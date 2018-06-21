b8872adf0fa245a17243cabc32d2e5a64f3bf3a6
BJ Neilsen <bj.neilsen@gmail.com>
2018-06-21 11:22:58 -0400

    Ensure preferredOperators support in GroupTypeahead, add IS ONE OF operator support for GROUP:GROUP_LIST

--- src/data-dictionary/operators/is-one-of.js
+++ src/data-dictionary/operators/is-one-of.js
@@ -1,13 +1,12 @@
-import {
-  TEXT,
-  USER
-} from '../return-types'
+import _ from 'lodash'
+import { GROUP, GROUP_LIST } from '../return-types'
+
+const equalityById = _.curry((left, right) =>
+  _.isEqual(_.get(left, 'id'), _.get(right, 'id'))
+)
 
 export default {
-  [TEXT]: {
-    [TEXT]: (left, right) => left === right
-  },
-  [USER]: {
-    [USER]: (left, right) => left.id === right.id
+  [GROUP]: {
+    [GROUP_LIST]: (group, groupList) => _.some(groupList, equalityById(group))
   }
 }
