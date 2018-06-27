1098c6ef9c4474ab2add4189dcf4e4add166a8a3
Thomas A. Valletta <tom@vallettas.com>
2018-06-27 15:55:38 -0600

    make core user multiselect work

--- src/data-dictionary/global-forms/field-core-user-multiselect.js
+++ src/data-dictionary/global-forms/field-core-user-multiselect.js
@@ -6,17 +6,39 @@
  * Agreement with this file. If not, please write to license@kuali.co.
  */
 import Field from './field'
-import { USER, TEXT } from '../return-types'
+import { get } from 'lodash'
+import { USER, USER_LIST, TEXT, TEXT_LIST } from '../return-types'
+import { names, IS_EMPTY, IS_NOT_EMPTY } from '../operators'
 
 export default class FieldCoreUserMultiselect extends Field {
   static typeLabel = 'UserMultiselect'
   static type = 'field-core-user-multiselect'
-  static treatAsType = USER
-  static returnTypes = [USER, TEXT]
-  static matchTypes = [USER, TEXT]
-  static preferredOperators = []
+  static treatAsType = USER_LIST
+  static returnTypes = [USER, USER_LIST, TEXT, TEXT_LIST]
+  static matchTypes = [USER_LIST, TEXT_LIST]
+  static preferredOperators = names(IS_EMPTY, IS_NOT_EMPTY)
 
   getChildren = async filter => []
 
   isLeaf = () => true
+
+  /**
+   * Expect the parent form to have provided a document containing values
+   * @param {Object} valueMap a map of parent's output
+   */
+  async getValue (valueMap = {}) {
+    const { data, parent } = this
+    if (parent) await parent.getValue(valueMap)
+    if (!valueMap.formfill || !valueMap.formfill.document) return
+    const { document } = valueMap.formfill
+    console.log({ data, parent, document, valueMap })
+    const userId = get(document, `data.${data.formKey}.id`)
+    console.log({ userId })
+    const user = userId ? await this.ctx.apis.users.getUser(userId) : {}
+    user.toString = function () {
+      return this.displayName || this.username
+    }
+    valueMap.field = { value: user }
+    return user
+  }
 }
